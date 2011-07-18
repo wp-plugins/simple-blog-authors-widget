f4ffea38944436522925eaae533ac31bbfa2a9a7
mordauk <mordauk@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-07-18 19:07:51 +0000

    adding version 1.0
    git-svn-id: https://plugins.svn.wordpress.org/simple-blog-authors-widget/trunk@411614 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ simple-blog-authors-widget/readme.txt
@@ -0,0 +1,37 @@
+=== Simple Blog Authors Widget ===
+Author: Pippin Williamson
+Author URI: http://pippinsplugins.com
+Plugin URI: http://pippinspages.com/simple-blog-authors-widget/
+Donate link: https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=72HGD7SA97KPE
+Tags: widget, widgets, authors, blog authors, simple
+Requires at least: 2.8
+Tested up to: 3.2.1
+Stable tag: 1.0
+
+This plugin lets provides a simple widget to list your blog's authors, including gravatar and post counts
+
+== Description ==
+
+This plugin will give you a simple widget that you can use to display your blog's authors, including their gravatars and post counts.
+
+The widget uses universal widget styling, so should fit perfectly into any WordPress theme.
+
+== Features ==
+
+1. Blog Authors Widget
+2. Widget Title Option
+3. Option to enable author gravatars
+4. Option to enable author post counts
+
+== Installation ==
+
+1. Upload the 'simple-blog-authors-widget' folder to the `/wp-content/plugins/` directory
+2. Activate the plugin through the 'Plugins' menu in WordPress
+
+== Screenshots ==
+
+Check out http://pippinsplugins.com/simple-blog-authors-widget/
+
+
+
+
---
+++ simple-blog-authors-widget/simple-blog-authors-widget.php
@@ -0,0 +1,108 @@
+<?php
+/*
+Plugin Name: Simple Blog Authors Widget
+Plugin URI: http://pippinsplugins.com/simple-blog-authors-widget
+Description: Provides a widget to list blog authors, including gravatars, post counts, and bios
+Version: 1.0
+Author: Pippin Williamson
+Author URI: http://pippinsplugins.com
+*/
+
+/**
+ * Authors Widget Class
+ */
+class pippin_simple_authors_widget extends WP_Widget {
+
+
+    /** constructor */
+    function pippin_simple_authors_widget() {
+        parent::WP_Widget(false, $name = 'Simple Authors Widget');	
+    }
+
+    /** @see WP_Widget::widget */
+    function widget($args, $instance) {	
+        extract( $args );
+		global $wpdb;
+		
+        $title = apply_filters('widget_title', $instance['title']);
+		$gravatar = $instance['gravatar'];
+		$count = $instance['count'];
+		
+		if(!$size)
+			$size = 40;
+
+        ?>
+              <?php echo $before_widget; ?>
+                  <?php if ( $title )
+                        echo $before_title . $title . $after_title; ?>
+							<ul>
+							<?php
+
+								$authors = $wpdb->get_results("SELECT ID FROM $wpdb->users ORDER BY ID");
+
+								foreach($authors as $author) {
+									
+									$author_info = get_userdata($author->ID);
+									
+									echo '<li>';
+									
+										echo '<div style="float: left; margin-left: 5px;">';
+
+										echo get_avatar($author->ID, 40);
+
+										echo '</div>';
+
+										echo '<a href="' . get_author_posts_url($author->ID) .'" title="View author archive">';
+											echo $author_info->display_name;
+											if($count) {
+												echo '(' . count_user_posts($author->ID) . ')';
+											}
+										echo '</a>';
+
+									echo '</li>';
+								}							
+							?>
+							</ul>
+              <?php echo $after_widget; ?>
+        <?php
+    }
+
+    /** @see WP_Widget::update */
+    function update($new_instance, $old_instance) {		
+		$instance = $old_instance;
+		$instance['title'] = strip_tags($new_instance['title']);
+		$instance['gravatar'] = strip_tags($new_instance['gravatar']);
+		$instance['count'] = strip_tags($new_instance['count']);
+        return $instance;
+    }
+
+    /** @see WP_Widget::form */
+    function form($instance) {	
+
+        $title = esc_attr($instance['title']);
+		$gravatar = esc_attr($instance['gravatar']);
+		$count = esc_attr($instance['count']);
+		
+        ?>
+         <p>
+          <label for="<?php echo $this->get_field_id('title'); ?>"><?php _e('Title:'); ?></label> 
+          <input class="widefat" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" type="text" value="<?php echo $title; ?>" />
+        </p>
+
+		<p>
+          <input id="<?php echo $this->get_field_id('count'); ?>" name="<?php echo $this->get_field_name('count'); ?>" type="checkbox" value="1" <?php checked( '1', $count ); ?>/>
+          <label for="<?php echo $this->get_field_id('count'); ?>"><?php _e('Display Post Count?'); ?></label> 
+        </p>
+
+		<p>
+          <input id="<?php echo $this->get_field_id('gravatar'); ?>" name="<?php echo $this->get_field_name('gravatar'); ?>" type="checkbox" value="1" <?php checked( '1', $gravatar ); ?>/>
+          <label for="<?php echo $this->get_field_id('gravatar'); ?>"><?php _e('Display Author Gravatar?'); ?></label> 
+        </p>
+
+        <?php 
+    }
+
+
+} // class utopian_recent_posts
+// register Recent Posts widget
+add_action('widgets_init', create_function('', 'return register_widget("pippin_simple_authors_widget");'));
